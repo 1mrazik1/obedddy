42d4c9f21141af43c3c3d8a3eb992e54b20905b8
user <user@LENOVO-PC>
2018-05-14 20:24:39 +0200

    root

--- obedddy/db.php
+++ obedddy/db.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "obedy");
+$mysqli = new mysqli("localhost", "root", "root", "obedy");
 if ($mysqli->connect_errno) {
     echo "Failed to connect to MySQL: ({$mysqli->connect_errno}) {$mysqli->connect_error}";
 }
